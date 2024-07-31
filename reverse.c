a6dfd4ebeef860d4a1c2cfa01937d989f4ad4a6e
VaishaliG11 <22104027@MAIL.JIIT.AC.IN>
2024-07-31 09:29:28 +0530

    Reverse a string code

---
+++ reverse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverseString(char str[])
+{
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
+
+int main()
+{
+    char str[200];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+
+
+    return 0;
+}
